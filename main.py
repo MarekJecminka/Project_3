0a5d93eef91c4f52f9316016d621fb48ab21609e
MarekJecminka <jecminkam@seznam.cz>
2026-01-24 03:00:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -32,14 +32,14 @@ def vysledky_hlasovani():
         return okresy
 
     def vytvor_jmena_csv(okresy_url):
-        vsechna_mesta = ["vysledky_praha.csv"]
+        vsechny_okresy = ["vysledky_praha.csv"]
         for item in okresy_url:
             rozdelene_html = bs(get(item).text, features="html.parser")
             vsechny_h3 = rozdelene_html.find_all("h3")
             for tag in vsechny_h3:
                 if "Okres: " in str(tag):
-                    vsechna_mesta.append(f"vysledky_{str(tag)[12:-6].lower()}.csv")
-        return vsechna_mesta    
+                    vsechny_okresy.append(f"vysledky_{str(tag)[12:-6].lower()}.csv")
+        return vsechny_okresy    
 
     def odstran_diakritiku(jmena_okresu_csv):
         bez_diakritiky = list()
@@ -88,6 +88,24 @@ def vysledky_hlasovani():
             else:
                 return False
     
+    def ziskej_code():
+        url_okresu = [sys.argv[1]]
+        rozdelene_html = bs(get(url_okresu).text, features="html.parser")
+        vsechny_table = rozdelene_html.find_all("table", {"class": "table"})
+        code = dict()
+        for table in vsechny_table:
+            vsechny_tr = table.find_all("tr")
+            for tr in vsechny_tr[2:]:
+                td_na_radku = tr.find_all("td")
+                if td_na_radku[0].text != "-":
+                    code["code"] = td_na_radku[0].text
+        return code
+
+    def uloz_volebni_data():
+        code = ziskej_code()
+        
+        return
+
     print("Ověřuji systémové argumenty...")
     okresy = najdi_uzemni_celky()
     jmena_okresu_csv = vytvor_jmena_csv(okresy)
@@ -95,20 +113,26 @@ def vysledky_hlasovani():
     prihlasovaci_udaje = vytvor_prihlasovaci_udaje(okresy, jmena_okresu_csv_bez_diakritiky)
     spravne_udaje = over_prihlasovaci_udaje(prihlasovaci_udaje)
     
-    if spravne_udaje == True:
+    if spravne_udaje:
         print("Ukládám data.")
+        uloz_volebni_data()
     else:
         print("Nesprávné systémové argumenty. Vlož správné systémové argumenty.")
 
 vysledky_hlasovani()
 
 
-#Nefunguje případ, kdy systemový argument 1 je prázdný - OPRAVIT!!
 
-#python a.py "https://www.volby.cz/pls/ps2017nss/ps32?xjazyk=CZ&xkraj=1&xnumnuts=1100" "vysledky_praha.csv"
+#python project_3.py "https://www.volby.cz/pls/ps2017nss/ps32?xjazyk=CZ&xkraj=1&xnumnuts=1100" "vysledky_praha.csv"
 
-#https://www.volby.cz/pls/ps2017nss/ps32?xjazyk=CZ&xkraj=2&xnumnuts=2101 - okres Benešov
+#python project_3.py "https://www.volby.cz/pls/ps2017nss/ps32?xjazyk=CZ&xkraj=2&xnumnuts=2101" "vysledky_benesov.csv"
 #https://www.volby.cz/pls/ps2017nss/ps311?xjazyk=CZ&xkraj=2&xobec=529303&xvyber=2101 - obec Benešov
+
+
+
+
+
+
 
 
 from requests import get
@@ -145,17 +169,111 @@ url_obce = "https://www.volby.cz/pls/ps2017nss/ps311?xjazyk=CZ&xkraj=3&xnumnuts=
 #https://www.volby.cz/pls/ps2017nss/ps32?xjazyk=CZ&xkraj=2&xnumnuts=2101 - okres Benešov
 #https://www.volby.cz/pls/ps2017nss/ps311?xjazyk=CZ&xkraj=2&xobec=529303&xvyber=2101 - obec Benešov
 
-url_okresu = "https://www.volby.cz/pls/ps2017nss/ps32?xjazyk=CZ&xkraj=2&xnumnuts=2103"
-rozdelene_html = bs(get(url_okresu).text, features="html.parser")
-vsechny_table = rozdelene_html.find_all("table", {"class": "table"})
-cisla_a_obce = []
-for table in vsechny_table:
+#url_okresu = "https://www.volby.cz/pls/ps2017nss/ps32?xjazyk=CZ&xkraj=2&xnumnuts=2103"
+#rozdelene_html = bs(get(url_okresu).text, features="html.parser")
+#vsechny_table = rozdelene_html.find_all("table", {"class": "table"})
+#cisla_a_obce = []
+#for table in vsechny_table:
+#    vsechny_tr = table.find_all("tr")
+#    for tr in vsechny_tr[2:]:
+#        td_na_radku = tr.find_all("td")
+#        if td_na_radku[0].text != "-":
+#            cisla_a_obce.append(td_na_radku[0].text)
+#        if td_na_radku[1].text != "-":
+#            cisla_a_obce.append(td_na_radku[1].text)
+
+#print(cisla_a_obce)
+
+
+url_obce = "https://www.volby.cz/pls/ps2017nss/ps311?xjazyk=CZ&xkraj=1&xobec=500054&xvyber=1100"
+rozdelene_html = bs(get(url_obce).text, features="html.parser")
+vsechny_table = rozdelene_html.find_all("table", {"class":"table"})
+#vsechny_tr = vsechny_table[0].find_all("tr")
+#td_na_radku = vsechny_tr[2].find_all("td")
+volebni_ucast = dict()
+#volebni_ucast["registered"] = str(td_na_radku[3].text)
+#volebni_ucast["envelopes"] = str(td_na_radku[4].text)
+#volebni_ucast["valid"] = str(td_na_radku[7].text)
+
+text = "7\\xa258"
+if "\\xa" in text:
+    novy_text = text.replace("\\xa", "")
+
+tabulka_s_hlasy = vsechny_table[1:]
+for table in tabulka_s_hlasy:
     vsechny_tr = table.find_all("tr")
     for tr in vsechny_tr[2:]:
         td_na_radku = tr.find_all("td")
-        if td_na_radku[0].text != "-":
-            cisla_a_obce.append(td_na_radku[0].text)
         if td_na_radku[1].text != "-":
-            cisla_a_obce.append(td_na_radku[1].text)
+            volebni_ucast[td_na_radku[1].text] = td_na_radku[2].text
 
-print(cisla_a_obce)
+print(volebni_ucast)
+
+
+
+
+
+
+
+
+
+
+from requests import get
+import sys
+from bs4 import BeautifulSoup as bs
+
+#url = "https://www.volby.cz/pls/ps2017nss/ps32?xjazyk=CZ&xkraj=2&xnumnuts=2101"
+#base_url = "https://www.volby.cz/pls/ps2017nss/"
+
+#rozdelene_html = bs(get(url).text, features="html.parser")
+#a_tagy = rozdelene_html.find_all("a")
+
+#cisla_obci = list()
+
+#for a_tag in a_tagy:
+#    if "ps311" in str(a_tag):
+#        cisla_obci.append(a_tag.attrs.get("href", "chybí odkaz"))
+
+#for neco in cisla_obci:
+#    print(base_url + neco)
+
+
+#https://www.volby.cz/pls/ps2017nss/ps32?xjazyk=CZ&xkraj=2&xnumnuts=2101 - okres Benešov
+#https://www.volby.cz/pls/ps2017nss/ps311?xjazyk=CZ&xkraj=2&xobec=529303&xvyber=2101 - obec Benešov
+
+#url_okresu = "https://www.volby.cz/pls/ps2017nss/ps32?xjazyk=CZ&xkraj=2&xnumnuts=2103"
+#rozdelene_html = bs(get(url_okresu).text, features="html.parser")
+#vsechny_table = rozdelene_html.find_all("table", {"class": "table"})
+#code = dict()
+#location = dict()
+#for table in vsechny_table:
+#    vsechny_tr = table.find_all("tr")
+#    for tr in vsechny_tr[2:]:
+#        td_na_radku = tr.find_all("td")
+#        if td_na_radku[0].text != "-":
+#            code["code"] = td_na_radku[0].text
+#        if td_na_radku[1].text != "-":
+#            location["location"] = td_na_radku[1].text
+
+#print(cisla_a_obce)
+
+
+url_obce = "https://www.volby.cz/pls/ps2017nss/ps311?xjazyk=CZ&xkraj=1&xobec=500054&xvyber=1100"
+rozdelene_html = bs(get(url_obce).text, features="html.parser")
+vsechny_table = rozdelene_html.find_all("table", {"class":"table"})
+#vsechny_tr = vsechny_table[0].find_all("tr")
+#td_na_radku = vsechny_tr[2].find_all("td")
+volebni_ucast = dict()
+#volebni_ucast["registered"] = str(td_na_radku[3].text)
+#volebni_ucast["envelopes"] = str(td_na_radku[4].text)
+#volebni_ucast["valid"] = str(td_na_radku[7].text)
+
+tabulka_s_hlasy = vsechny_table[1:]
+for table in tabulka_s_hlasy:
+    vsechny_tr = table.find_all("tr")
+    for tr in vsechny_tr[2:]:
+        td_na_radku = tr.find_all("td")
+        if td_na_radku[1].text != "-":
+            volebni_ucast[td_na_radku[1].text] = td_na_radku[2].text
+
+print(volebni_ucast)
