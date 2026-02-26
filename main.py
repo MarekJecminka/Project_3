0075dc5124126ddf0d41f90c59a4f49a254b9a47
MarekJecminka <jecminkam@seznam.cz>
2026-02-26 19:16:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -223,16 +223,14 @@ from requests import get
 from bs4 import BeautifulSoup as bs
 
 def vysledky_hlasovani():
-    base_url = "https://www.volby.cz/pls/ps2017nss/"
-    url = "https://www.volby.cz/pls/ps2017nss/ps3?xjazyk=CZ"
 
-    def najdi_uzemni_celky():
-        rozdelene_html = bs(get(url).text, features="html.parser")
+    def najdi_uzemni_celky(url: str) -> list:
+        rozdelene_html = bs(get("https://www.volby.cz/pls/ps2017nss/ps3?xjazyk=CZ").text, features="html.parser")
         vsechny_celky = rozdelene_html.find_all("a")
         celky = [a_tag.attrs.get("href", "chybí odkaz") for a_tag in vsechny_celky if "xnumnuts" in str(a_tag)]
-        return [base_url + item for item in celky if "ps32" in item]
+        return [url + item for item in celky if "ps32" in item]
 
-    def vytvor_jmena_csv(okresy_url):
+    def vytvor_jmena_csv(okresy_url: list) -> list:
         vsechny_okresy = ["vysledky_praha.csv"]
         for item in okresy_url:
             rozdelene_html = bs(get(item).text, features="html.parser")
@@ -242,7 +240,7 @@ def vysledky_hlasovani():
                     vsechny_okresy.append(f"vysledky_{str(tag)[12:-6].lower()}.csv")
         return vsechny_okresy    
 
-    def odstran_diakritiku(jmena_okresu_csv):
+    def odstran_diakritiku(jmena_okresu_csv: list) -> list:
         bez_diakritiky = []
         odstranit_diakritiku = {"á":"a","č":"c","ď":"d","é":"e","ě":"e","í":"i","ň":"n","ó":"o",
                                 "ř":"r","š":"s","ť":"t","ú":"u","ů":"u","ý":"y","ž":"z"}
@@ -251,7 +249,7 @@ def vysledky_hlasovani():
             bez_diakritiky.append(csv)
         return bez_diakritiky
 
-    def over_prihlasovaci_udaje(udaje):
+    def over_prihlasovaci_udaje(udaje: dict) -> bool:
         try:
             udaje[sys.argv[1]] == sys.argv[2]
         except KeyError:
@@ -264,12 +262,11 @@ def vysledky_hlasovani():
             else:
                 return False
     
-    def najdi_linky_obci():
+    def najdi_linky_obci(base_url_obci: str) -> list:
         url = sys.argv[1]
         rozdelene_html = bs(get(url).text, features="html.parser")
         a_tagy = rozdelene_html.find_all("a")
-        base_url = "https://www.volby.cz/pls/ps2017nss/"
-        linky_obci = [base_url + a_tag.attrs.get("href") for a_tag in a_tagy if "ps311" in str(a_tag)]
+        linky_obci = [base_url_obci + a_tag.attrs.get("href") for a_tag in a_tagy if "ps311" in str(a_tag)]
         linky = []
         for link in linky_obci:
             if link in linky:
@@ -278,7 +275,7 @@ def vysledky_hlasovani():
                 linky.append(link)
         return linky
     
-    def najdi_code_a_location():
+    def najdi_code_a_location() -> list:
         url_okresu = sys.argv[1]
         rozdelene_html = bs(get(url_okresu).text, features="html.parser")
         vsechny_table = rozdelene_html.find_all("table", {"class": "table"})
@@ -295,7 +292,7 @@ def vysledky_hlasovani():
                 code_a_location_tabulek.append(code_a_location_radku)
         return code_a_location_tabulek
     
-    def najdi_volebni_ucast(linky_obci):
+    def najdi_volebni_ucast(linky_obci: list) -> list:
         volebni_ucast = []
         for link in linky_obci:
             url_obce = link
@@ -307,7 +304,7 @@ def vysledky_hlasovani():
             volebni_ucast.append(volebni_ucast_obce)
         return volebni_ucast
     
-    def najdi_hlasy_stran(linky_obci):
+    def najdi_hlasy_stran(linky_obci: list) -> list:
         hlasy_stran = []
         klice = []
         for link in linky_obci:
@@ -329,7 +326,7 @@ def vysledky_hlasovani():
             klice.append(klice_obce)
         return [hlasy_stran, klice[0]]
 
-    def spoj_data_obci(code_a_location_obci, volebni_ucast_obci, hlasy_stran_obci):
+    def spoj_data_obci(code_a_location_obci: list, volebni_ucast_obci: list, hlasy_stran_obci: list) -> list:
         data_obci = []
         for code, ucast, hlasy in zip(code_a_location_obci, volebni_ucast_obci, hlasy_stran_obci):
             data_obce = []
@@ -342,14 +339,14 @@ def vysledky_hlasovani():
             data_obci.append(data_obce)
         return data_obci
 
-    def prirad_klice_k_datum(klice_dat, data):
+    def prirad_klice_k_datum(klice_dat: list, data: list) -> list[dict]:
         data_obci = []
         for obec in data:
             data_obce = {klic: hodnota for klic, hodnota in zip(klice_dat, obec)}
             data_obci.append(data_obce)
         return data_obci
     
-    def zapis_data_do_csv(data_csv, klice_csv):
+    def zapis_data_do_csv(data_csv: list[dict], klice_csv: list):
         soubor_csv = open(f"{sys.argv[2]}", mode="w", newline="", encoding="utf-8-sig")
         zahlavi = klice_csv
         zapisovac = csv.DictWriter(soubor_csv, fieldnames=zahlavi)
@@ -357,8 +354,8 @@ def vysledky_hlasovani():
         zapisovac.writerows(data_csv)
         soubor_csv.close()
 
-    def uloz_volebni_data():
-        linky = najdi_linky_obci()
+    def uloz_volebni_data(base_url_dat):
+        linky = najdi_linky_obci(base_url_dat)
         code_a_location = najdi_code_a_location()
         volebni_ucast = najdi_volebni_ucast(linky)
         hlasy_stran = najdi_hlasy_stran(linky)[0]
@@ -368,7 +365,8 @@ def vysledky_hlasovani():
         zapis_data_do_csv(data, klice)
 
     print("Ověřuji systémové argumenty...")
-    okresy = najdi_uzemni_celky()
+    base_url = "https://www.volby.cz/pls/ps2017nss/"
+    okresy = najdi_uzemni_celky(base_url)
     jmena_okresu_csv = vytvor_jmena_csv(okresy)
     jmena_okresu_csv_bez_diakritiky = odstran_diakritiku(jmena_okresu_csv)
     prihlasovaci_udaje = {klic: hodnota for klic, hodnota in zip(okresy, jmena_okresu_csv_bez_diakritiky)}
@@ -376,7 +374,7 @@ def vysledky_hlasovani():
     
     if spravne_udaje:
         print("Ukládám data...")
-        uloz_volebni_data()
+        uloz_volebni_data(base_url)
         print("Ukládání dokončeno.")
     else:
         print("Nesprávné systémové argumenty. Vlož správné systémové argumenty.")
